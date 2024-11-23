c62872c24d4c59e5ddd98f282f651b4a33f49cfd
JamesGough24 <146301612+JamesGough24@users.noreply.github.com>
2024-11-23 17:25:54 -0500

    Implemented scraping of advanced match metrics

--- src/utils/scraper.py
+++ src/utils/scraper.py
@@ -119,10 +119,70 @@ def scrape_match(url: str):
     stats_page_button.click()
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
-    
+
+    # home_npxg, away_npxg = scrape_stat("Non-penalty xG", soup)
+    # away_npxga = home_npxg
+    # home_npxga = away_npxg
+
+    # home_bcc, away_bcc = scrape_stat("Big chances", soup)
+    # away_bca = home_bcc
+    # home_bca = away_bcc
+
+    # home_xg, away_xg = scrape_stat("Expected goals (xG)", soup)
+    # home_xgot, away_xgot = scrape_stat("xG on target (xGOT)", soup)
+
+    # print(f"home_xg = {home_xg}")
+    # print(f"away_xg = {away_xg}")
+
+    scrape_head_2_head(driver, soup)
 
     driver.quit()
     return
+
+def scrape_stat(stat: str, soup: BeautifulSoup):
+    # Finds span element with provided stat text
+    span = soup.find("span", string=stat)
+    # Gets the parent element containing the divs for the stat values
+    li_tag = span.find_parent('li')
+    # Collects home and away values from their respective divs and removes potential whitespace
+    home_val = float(li_tag.select_one("div.css-129ncdm-StatBox span span").text.strip())
+    away_val = float(li_tag.select_one("div.css-1t9answ-StatBox span span").text.strip())
+
+    return home_val, away_val
+
+def scrape_head_2_head(driver: webdriver.Chrome, soup: BeautifulSoup):
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "Head-to-Head")]'))
+    )
+
+    head_2_head_page_button = driver.find_element(By.XPATH, '//button[contains(text(), "Head-to-Head")]')
+    head_2_head_page_button.click()
+
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "Home")]'))
+    )
+
+    home_team_button = driver.find_element(By.XPATH, '//button[contains(text(), "Home")]')
+    home_team_button.click()
+
+    head_2_head_div = soup.select(".css-1e2kjkx-H2hNumbers e3lka4k2")
+    print(head_2_head_div)
+
+    home_wins = int(head_2_head_div[0].select_one("[class*='NumberOfWins'] span").text.strip())
+    draws = int(head_2_head_div[1].select_one("[class*='NumberOfWins'] span").text.strip())
+    away_wins = int(head_2_head_div[2].select_one("[class*='NumberOfWins'] span").text.strip())
+
+    print(home_wins)
+    print(draws)
+    print(away_wins)
+
+    total_meetings = home_wins + draws + away_wins
+
+    home_win_pct = home_wins / total_meetings
+    draw_pct = draws / total_meetings
+    away_win_pct = away_wins / total_meetings
+
+    return home_win_pct, draw_pct, away_win_pct
 
 def scrape_matches(league: str, season: str, gameweek: int):
     match_links = find_matches(league, season, gameweek)
@@ -131,3 +191,4 @@ def scrape_matches(league: str, season: str, gameweek: int):
         match_data = scrape_match(match)
     return
 
+scrape_match("https://www.fotmob.com/matches/osasuna-vs-real-madrid/2e2ylz#4506882:tab=stats")
